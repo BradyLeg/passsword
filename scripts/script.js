6b6eddcb8a319e84046554e3374ada5ac901b4bb
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-01-22 14:30:49 -0800

    added javascript to check if the password is less than 8 in length and contains no special characters

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,14 @@
 document.getElementById('click').onclick = function () {
     let password = document.getElementById('password').value;
 
+    if (password.length < 8) {
+        document.getElementById('output').innerHTML += "Password must be at least 8 characters long";
+    }
+
+    // Regexp test for no special characaters
+    if (/[^a-zA-Z0-9]/.test(password)) {
+        document.getElementById('output').innerHTML += "Password must contain at least 1 special character";
+    }
+
 }
+
